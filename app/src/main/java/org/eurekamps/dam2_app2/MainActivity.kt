7143dca6758df75fd06e5918cfd9763ee53656bd
Eurekamps <4yony4@gmail.com>
2024-09-22 13:47:59 +0200

    Initial Commit

---
+++ app/src/main/java/org/eurekamps/dam2_app2/MainActivity.kt
@@ -0,0 +1,41 @@
+package org.eurekamps.dam2_app2
+
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(),OnClickListener {
+
+    lateinit var btnLogin:Button
+    lateinit var btnRegistrar:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnLogin=findViewById(R.id.button)
+        btnRegistrar=findViewById(R.id.button2)
+        btnLogin.setOnClickListener(this)
+        btnRegistrar.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        if(p0!!.id==btnLogin.id){
+            println("!!!!!!!!!!!------>>>>>>>>>> BOTON LOGIN")
+        }
+        else if(p0!!.id==btnRegistrar.id){
+            println("!!!!!!!!!!!------>>>>>>>>>> BOTON REGISTRO")
+        }
+
+
+    }
+
+
+}
